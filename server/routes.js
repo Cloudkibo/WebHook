bd8541b23708c720a541b4c990e48d92e03a2f2d
Baqar <bjafri5@gmail.com>
2018-09-14 15:12:53 +0500

    restructure location of webhook files

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
 
   // app.use('/api/v1/things', require('./api/v1/thing'))
   // app.use('/webhooks/messenger', require('./webhook_subscriptions/messenger'))
-  // app.use('/webhooks/wordpress', require('./webhook_subscriptions/wordpress'))
+  app.use('/webhooks/wordpress', require('./webhook_subscriptions/wordpress'))
 
   app.route('/:url(api|auth)/*').get((req, res) => {
     res.status(404).send({url: `${req.originalUrl} not found`})
