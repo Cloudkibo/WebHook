f2a1053eccf6243c239c387e996fbaea581e30c8
Baqar <bjafri5@gmail.com>
2018-09-25 16:45:26 +0500

    registered webhooks

--- server/webhook_subscriptions/wordpress/webhooks.js
+++ server/webhook_subscriptions/wordpress/webhooks.js
@@ -2,7 +2,7 @@ const TAG = 'webhook_subscriptions/wordpress/webhook.js'
 const logger = require('../../components/logger')
 const callApi = require('../../utility/api.caller.service')
 
-exports.publishPostWebhook = (payload) => {
+exports.publishPost = (payload) => {
   logger.serverLog(TAG, `in publishPostWebhook ${JSON.stringify(payload)}`)
   callApi.callApi('/wordpressEvents/publishPost', 'post', payload)
 }
