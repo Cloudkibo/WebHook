7dc2c722d41889c9c95c6fdf3fb2af796830988d
Baqar <bjafri5@gmail.com>
2018-09-26 16:37:51 +0500

    add authorization to twitter restart

--- server/webhook_subscriptions/twitter/index.js
+++ server/webhook_subscriptions/twitter/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require('./twitter.controller')
+const auth = require('../../../server/auth/auth.service')
 
-router.get('/restart', controller.restart)
+router.get('/restart', auth.isAuthorizedWebHookTrigger(), controller.restart)
 
 router.get('/', controller.verifyHook)
 router.post('/', controller.webhook)
