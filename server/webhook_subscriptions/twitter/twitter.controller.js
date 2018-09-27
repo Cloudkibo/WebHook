bae5ae864bd1ce1bc4c5ef0d3b6c630c30079c99
anishachhatwani <i130315@nu.edu.pk>
2018-09-28 00:01:11 +0500

    #16

--- server/webhook_subscriptions/twitter/twitter.controller.js
+++ server/webhook_subscriptions/twitter/twitter.controller.js
@@ -70,10 +70,10 @@ function connect () {
         {follow: arrUsers})
 
       stream.on('tweet', tweet => {
-        logger.serverLog(TAG, `received new tweet`)
         if (tweet.in_reply_to_status_id !== null || tweet.in_reply_to_user_id !== null || tweet.in_reply_to_screen_name !== null) {
           return
         }
+        logger.serverLog(TAG, `received new tweet`)
         webhookHandler(tweet)
       })
     }
