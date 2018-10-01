e94623e2a44a0cf70a5729c5730766f30afc220f
Baqar <bjafri5@gmail.com>
2018-10-01 16:49:48 +0500

    added promise handling

--- server/webhook_subscriptions/messenger/getStarted/getStartedWebhook.js
+++ server/webhook_subscriptions/messenger/getStarted/getStartedWebhook.js
@@ -6,4 +6,10 @@ exports.getStartedWebhook = (payload) => {
   logger.serverLog(TAG,
     `in getStartedWebhook ${JSON.stringify(payload)}`)
   callApi.callApi('messengerEvents/subscriber', 'post', payload)
+        .then((response) => {
+          logger.serverLog(TAG, `response recieved from KiboPush: ${response}`)
+        })
+    .catch((err) => {
+      logger.serverLog(TAG, `error from KiboPush: ${err}`)
+    })
 }
--- server/webhook_subscriptions/messenger/schemas.js
+++ server/webhook_subscriptions/messenger/schemas.js
@@ -29,3 +29,33 @@ exports.testSchema = {
     'required': ['votes']
   }
 }
+
+exports.testSchema1 = {
+  'type': 'object',
+  'properties': {
+    'name': {'type': 'string'},
+    'votes': {'type': 'integer', 'minimum': 1},
+    'user': {
+      'type': 'object',
+      'properties': {
+        'id': {
+          'type': 'integer'
+        },
+        'id2': {
+          'type': 'integer'
+        },
+        'id_str': {
+          'type': 'string'
+        },
+        'name': {
+          'type': 'string'
+        },
+        'screen_name': {
+          'type': 'string'
+        }
+      },
+      'required': ['id', 'id2']
+    },
+    'required': ['votes']
+  }
+}
