cc9005e3278a82c06286d359593c9b2a92c18cf7
anishachhatwani <i130315@nu.edu.pk>
2018-10-07 18:48:49 +0500

    accounts work

--- server/webhook_subscriptions/messenger/newSubscriber/newSubscriberWebhook.js
+++ server/webhook_subscriptions/messenger/newSubscriber/newSubscriberWebhook.js
@@ -3,7 +3,7 @@ const logger = require('../../../components/logger')
 const callApi = require('../../../utility/api.caller.service')
 
 exports.newSubscriberWebhook = (payload) => {
-  if (!payload.entry[0].messaging[0].prior_message && !payload.entry[0].messaging[0].message.attachments && !payload.entry[0].messaging[0].postback && !payload.entry[0].messaging[0].delivery) {
+  if (!payload.entry[0].messaging[0].prior_message && payload.entry[0].messaging[0].message && !payload.entry[0].messaging[0].message.attachments && !payload.entry[0].messaging[0].postback && !payload.entry[0].messaging[0].delivery) {
     logger.serverLog(TAG,
       `in newSubscriberWebhook ${JSON.stringify(payload)}`)
     callApi.callApi('messengerEvents/subscriber', 'post', payload)
