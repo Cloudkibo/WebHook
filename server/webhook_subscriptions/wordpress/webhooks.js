545ec059ac9e72718c558dbade3651e8d934e1d8
sania <sania@cloudkibo.com>
2018-11-14 19:04:19 +0500

    Webhook Wordpress

--- server/webhook_subscriptions/wordpress/webhooks.js
+++ server/webhook_subscriptions/wordpress/webhooks.js
@@ -1,6 +1,6 @@
 const TAG = 'webhook_subscriptions/wordpress/webhook.js'
 const logger = require('../../components/logger')
-const callApi = require('../../utility/api.caller.service')
+const callApi = require('../../../utility/api.caller.service')
 
 exports.publishPost = (payload) => {
   logger.serverLog(TAG, `in publishPostWebhook ${JSON.stringify(payload)}`)
