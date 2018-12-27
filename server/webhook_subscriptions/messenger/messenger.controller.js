ed35ca9486a7c062ef7378002fb0e6bbaf824fa3
anishachhatwani <i130315@nu.edu.pk>
2018-12-27 10:09:12 +0500

    logs

--- server/webhook_subscriptions/messenger/messenger.controller.js
+++ server/webhook_subscriptions/messenger/messenger.controller.js
@@ -15,6 +15,7 @@ exports.verifyHook = function (req, res) {
 
 exports.webhook = function (req, res) {
   logger.serverLog(TAG, `something received from facebook ${JSON.stringify(req.body)}`)
+  console.log('something received from facebook', JSON.stringify(req.body))
   try {
     let webhookCalled = webhookHandler(req.body)
 
