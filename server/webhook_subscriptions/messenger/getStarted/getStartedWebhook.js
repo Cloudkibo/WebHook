5abab48b969af453f2962d29f0ce1110150cdaa8
anishachhatwani <i130315@nu.edu.pk>
2019-01-15 12:04:46 +0500

    getting started

--- server/webhook_subscriptions/messenger/getStarted/getStartedWebhook.js
+++ server/webhook_subscriptions/messenger/getStarted/getStartedWebhook.js
@@ -1,6 +1,8 @@
 const TAG = 'webhook_subscriptions/messenger/getStartedWebhook.js'
 const logger = require('../../../components/logger')
 const callApi = require('../../../utility/api.caller.service')
+const logicLayer = require('./logiclayer')
+const request = require('request')
 
 exports.getStartedWebhook = (payload) => {
   if (payload.entry[0].messaging[0].postback.referral) {
@@ -27,6 +29,53 @@ exports.getStartedWebhook = (payload) => {
   } else if (payload.entry[0].messaging[0].postback.payload === '<GET_STARTED_PAYLOAD>') {
     logger.serverLog(TAG,
       `in getStartedWebhook ${JSON.stringify(payload)}`)
-    callApi.callApi('messengerEvents/subscriber', 'post', payload, 'accounts')
+    sendWelcomeMessage(payload)
   }
+}
+
+function sendWelcomeMessage (payload) {
+  const sender = payload.entry[0].messaging[0].sender.id
+  const pageId = payload.entry[0].messaging[0].recipient.id
+  callApi.callApi(`pages/query`, 'post', { pageId: pageId, connected: true }, 'accounts')
+    .then(page => {
+      page = page[0]
+      console.log('page Found', page)
+      callApi.callApi(`subscribers/query`, 'post', { pageId: page._id, senderId: sender }, 'accounts')
+        .then(subscriber => {
+          subscriber = subscriber[0]
+          console.log('page._id', page._id)
+          if (page.welcomeMessage) {
+            for (let i = 0; i < page.welcomeMessage.length; i++) {
+              let messageData = logicLayer.prepareSendAPIPayload(subscriber.senderId, page.welcomeMessage[i], subscriber.firstName, subscriber.lastName, true)
+              console.log('messageData', messageData)
+              request(
+                {
+                  'method': 'POST',
+                  'json': true,
+                  'formData': messageData,
+                  'uri': 'https://graph.facebook.com/v2.6/me/messages?access_token=' +
+                    subscriber.pageId.accessToken
+                },
+                (err, res) => {
+                  if (err) {
+                    console.log(`At send message welcomeMessage ${JSON.stringify(err)}`)
+                  } else {
+                    console.log('res', res.body)
+                    if (res.statusCode !== 200) {
+                      logger.serverLog(TAG,
+                        `At send message landingPage ${JSON.stringify(
+                          res.body.error)}`)
+                    }
+                  }
+                })
+            }
+          }
+        })
+        .catch(err => {
+          logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(err)}`)
+        })
+    })
+    .catch(err => {
+      logger.serverLog(TAG, `Failed to fetch page ${JSON.stringify(err)}`)
+    })
 }
