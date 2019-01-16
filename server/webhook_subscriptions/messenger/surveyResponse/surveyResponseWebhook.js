a16cc78f31237a5c5b7f62d5488465bfda4d0d1d
anishachhatwani <i130315@nu.edu.pk>
2019-01-16 13:41:29 +0500

    unsubscribe

--- server/webhook_subscriptions/messenger/surveyResponse/surveyResponseWebhook.js
+++ server/webhook_subscriptions/messenger/surveyResponse/surveyResponseWebhook.js
@@ -1,6 +1,7 @@
 const TAG = 'webhook_subscriptions/messenger/surveyResponseWebhook.js'
 const logger = require('../../../components/logger')
 const callApi = require('../../../utility/api.caller.service')
+const needle = require('needle')
 
 exports.surveyResponseWebhook = (payload) => {
   if (payload.entry[0].messaging[0].postback.payload !== '<GET_STARTED_PAYLOAD>') {
@@ -16,13 +17,7 @@ exports.surveyResponseWebhook = (payload) => {
       logger.serverLog(TAG, `error from KiboPush: ${err}`)
     })
     } else if (resp.unsubscribe) {
-      callApi.callApi('messengerEvents/unsubscribe', 'post', payload, 'accounts')
-      .then((response) => {
-        logger.serverLog(TAG, `response recieved from KiboPush: ${response}`)
-      })
-    .catch((err) => {
-      logger.serverLog(TAG, `error from KiboPush: ${err}`)
-    })
+      handleUnsubscribe(resp, payload.entry[0].messaging[0])
     } else if (resp.action === 'subscribe') {
       callApi.callApi('messengerEvents/subscribeToSequence', 'post', payload, 'kiboengage')
       .then((response) => {
@@ -49,4 +44,60 @@ exports.surveyResponseWebhook = (payload) => {
     })
     }
   }
+}
+
+function handleUnsubscribe (resp, req) {
+  let messageData = {}
+  if (resp.action === 'yes') {
+    messageData = {
+      text: 'You have unsubscribed from our broadcasts. Send "start" to subscribe again.'
+    }
+    callApi.callApi(`subscribers/update`, 'put', {query: { senderId: req.sender.id }, newPayload: { isSubscribed: false }, options: {}}, 'accounts')
+      .then(updated => {
+        logger.serverLog(TAG, `updated subscriber: ${JSON.stringify(updated)}`)
+        callApi.callApi(`subscribers/query`, 'post', { senderId: req.sender.id }, 'accounts')
+          .then(subscribers => {
+            let subscriber = subscribers[0]
+            logger.serverLog(TAG, `subscriber fetched ${JSON.stringify(subscriber)}`)
+            callApi.callApi('featureUsage/updatePlanUsage', 'put', {query: {companyId: subscriber.companyId}, newPayload: { $inc: { subscribers: -1 } }, options: {}}, 'accounts')
+              .then(updated => {
+                logger.serverLog(TAG, `company usage incremented succssfully ${JSON.stringify(updated)}`)
+              })
+              .catch(err => {
+                logger.serverLog(TAG, `Failed to update company usage ${JSON.stringify(err)}`)
+              })
+          })
+          .catch(err => {
+            logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(err)}`)
+          })
+      })
+      .catch(err => {
+        logger.serverLog(TAG, `Failed to update subscriber ${JSON.stringify(err)}`)
+      })
+  } else {
+    messageData = {
+      text: 'You can unsubscribe anytime by saying stop.'
+    }
+  }
+  needle.get(
+    `https://graph.facebook.com/v2.10/${req.recipient.id}?fields=access_token&access_token=${resp.userToken}`,
+    (err3, response) => {
+      if (err3) {
+        logger.serverLog(TAG,
+          `Page token error from graph api ${JSON.stringify(err3)}`)
+      }
+      const data = {
+        messaging_type: 'RESPONSE',
+        recipient: JSON.stringify({ id: req.sender.id }), // this is the subscriber id
+        message: messageData
+      }
+      needle.post(
+        `https://graph.facebook.com/v2.6/me/messages?access_token=${response.body.access_token}`,
+        data, (err4, respp) => {
+          logger.serverLog(TAG, `ressp.body ${JSON.stringify(respp.body)}`)
+          logger.serverLog(TAG,
+            `Sending unsubscribe confirmation response to subscriber  ${JSON.stringify(
+              respp.body)}`)
+        })
+    })
 }
