9ee0918d6ce7632f7720e50d24c0ee6356d3114d
sania <sania@cloudkibo.com>
2019-01-16 15:43:48 +0500

    Accounts #231

--- server/webhook_subscriptions/messenger/getStarted/getStartedWebhook.js
+++ server/webhook_subscriptions/messenger/getStarted/getStartedWebhook.js
@@ -6,6 +6,7 @@ const request = require('request')
 const needle = require('needle')
 
 exports.getStartedWebhook = (payload) => {
+  logger.serverLog(TAG, `in getStartedWebhook ${JSON.stringify(payload)}`)
   if (payload.entry[0].messaging[0].postback.referral) {
     // This will send postback referal for messenger code
     logger.serverLog(TAG, `in Messenger ${JSON.stringify(payload)}`)
@@ -31,6 +32,28 @@ exports.getStartedWebhook = (payload) => {
     logger.serverLog(TAG,
       `in getStartedWebhook ${JSON.stringify(payload)}`)
     sendWelcomeMessage(payload)
+  }
+  var jsonPayload = payload.entry[0].messaging[0].postback.payload
+  logger.serverLog(TAG,
+    `in jsonAd ${JSON.stringify(jsonPayload)}`)
+  if (jsonPayload.payload) {
+    var jsonAdPayload = jsonPayload.payload.split('-')
+    logger.serverLog(TAG,
+      `in jsonAd ${JSON.stringify(jsonAdPayload[0])}`)
+    if (jsonAdPayload.length > 0) {
+      if (jsonAdPayload[0] === 'JSONAD') {
+        var jsonMessageId = jsonAdPayload[1]
+        callApi.callApi(`jsonAd/jsonAdResponse/${jsonMessageId}`, 'get', {}, 'accounts')
+          .then((response) => {
+            logger.serverLog(TAG,
+              `in Ad response ${JSON.stringify(response)}`)
+            sendResponseMessage(response)
+          })
+          .catch(err => {
+            logger.serverLog(TAG, `error from accounts jsonAdResponse: ${err}`)
+          })
+      }
+    }
   }
 }
 
@@ -128,5 +151,52 @@ function handleUnsubscribe (resp, req) {
             `Sending unsubscribe confirmation response to subscriber  ${JSON.stringify(
               respp.body)}`)
         })
+    })
+}
+
+function sendResponseMessage (payload, response) {
+  const sender = payload.entry[0].messaging[0].sender.id
+  const pageId = payload.entry[0].messaging[0].recipient.id
+  callApi.callApi(`pages/query`, 'post', { pageId: pageId, connected: true }, 'accounts')
+    .then(page => {
+      page = page[0]
+      console.log('page Found', page)
+      callApi.callApi(`subscribers/query`, 'post', { pageId: page._id, senderId: sender }, 'accounts')
+        .then(subscriber => {
+          subscriber = subscriber[0]
+          console.log('page._id', page._id)
+          if (response.messageContent) {
+            for (let i = 0; i < response.messageContent.length; i++) {
+              let messageData = logicLayer.prepareSendAPIPayload(subscriber.senderId, response.messageContent[i], subscriber.firstName, subscriber.lastName, true)
+              console.log('messageData', messageData)
+              request(
+                {
+                  'method': 'POST',
+                  'json': true,
+                  'formData': messageData,
+                  'uri': 'https://graph.facebook.com/v2.6/me/messages?access_token=' +
+                    subscriber.pageId.accessToken
+                },
+                (err, res) => {
+                  if (err) {
+                    console.log(`At send jsonAd response ${JSON.stringify(err)}`)
+                  } else {
+                    console.log('res', res.body)
+                    if (res.statusCode !== 200) {
+                      logger.serverLog(TAG,
+                        `At send message jsonAd response ${JSON.stringify(
+                          res.body.error)}`)
+                    }
+                  }
+                })
+            }
+          }
+        })
+        .catch(err => {
+          logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(err)}`)
+        })
+    })
+    .catch(err => {
+      logger.serverLog(TAG, `Failed to fetch page ${JSON.stringify(err)}`)
     })
 }
