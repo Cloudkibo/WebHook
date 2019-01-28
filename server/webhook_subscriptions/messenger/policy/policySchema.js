e0b18ae6d249db9ccec0116165c8249cd86f1ef8
Baqar <bjafri5@gmail.com>
2019-01-28 12:10:34 +0500

    policy and profile picture work

---
+++ server/webhook_subscriptions/messenger/policy/policySchema.js
@@ -0,0 +1,41 @@
+exports.policySchema = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'properties': {
+      'recipient': {
+        'type': 'object',
+        'properties': {
+          'id': {
+            'type': 'string'
+          }
+        },
+        'required': [
+          'id'
+        ]
+      },
+      'timestamp': {
+        'type': 'integer'
+      },
+      'policy-enforcement': {
+        'type': 'object',
+        'properties': {
+          'action': {
+            'type': 'string'
+          },
+          'reason': {
+            'type': 'string'
+          }
+        },
+        'required': [
+          'action',
+          'reason'
+        ]
+      }
+    },
+    'required': [
+      'recipient',
+      'timestamp',
+      'policy-enforcement'
+    ]
+  }
+  
