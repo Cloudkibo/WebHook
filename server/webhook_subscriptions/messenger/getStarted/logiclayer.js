1818c76443fe9b7075b53dfb39a38940495cc371
anishachhatwani <i130315@nu.edu.pk>
2019-02-13 13:13:41 +0500

    file uploading

--- server/webhook_subscriptions/messenger/getStarted/logiclayer.js
+++ server/webhook_subscriptions/messenger/getStarted/logiclayer.js
@@ -1,184 +1,201 @@
 const fs = require('fs')
 const path = require('path')
+const config = require('../../../config/environment/index')
+let request = require('request')
 
 function prepareSendAPIPayload (subscriberId, body, fname, lname, isResponse, jsonAdMessages) {
-  let messageType = isResponse ? 'RESPONSE' : 'UPDATE'
-  let payload = {}
-  let text = body.text
-  var buttonPayload = []
-  var button = {}
-  if (jsonAdMessages && body.buttons && body.buttons.length > 0) {
-    for (var i = 0; i < body.buttons.length; i++) {
-      button = body.buttons[i]
-      var jsonAdMessageId
-      if (button.payload && button.type === 'postback') {
-        for (var j = 0; j < jsonAdMessages.length; j++) {
-          if ((button.payload).toString() === (jsonAdMessages[j].jsonAdMessageId).toString()) {
-            jsonAdMessageId = jsonAdMessages[j]._id
-            break
+  return new Promise(function (resolve, reject) {
+    let messageType = isResponse ? 'RESPONSE' : 'UPDATE'
+    let payload = {}
+    let text = body.text
+    var buttonPayload = []
+    var button = {}
+    if (jsonAdMessages && body.buttons && body.buttons.length > 0) {
+      for (var i = 0; i < body.buttons.length; i++) {
+        button = body.buttons[i]
+        var jsonAdMessageId
+        if (button.payload && button.type === 'postback') {
+          for (var j = 0; j < jsonAdMessages.length; j++) {
+            if ((button.payload).toString() === (jsonAdMessages[j].jsonAdMessageId).toString()) {
+              jsonAdMessageId = jsonAdMessages[j]._id
+              break
+            }
           }
+          button.payload = 'JSONAD-' + jsonAdMessageId
         }
-        button.payload = 'JSONAD-' + jsonAdMessageId
+        buttonPayload.push(button)
       }
-      buttonPayload.push(button)
-    }
-  } else {
-    buttonPayload = body.buttons
-  }
-  if (body.componentType === 'text' && !body.buttons) {
-    if (body.text.includes('{{user_full_name}}') || body.text.includes('[Username]')) {
-      text = text.replace(
-        '{{user_full_name}}', fname + ' ' + lname)
-    }
-    if (body.text.includes('{{user_first_name}}')) {
-      text = text.replace(
-        '{{user_first_name}}', fname)
-    }
-    if (body.text.includes('{{user_last_name}}')) {
-      text = text.replace(
-        '{{user_last_name}}', lname)
-    }
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'text': text,
-        'metadata': 'This is a meta data'
-      })
-    }
-    return payload
-  } else if (body.componentType === 'text' && body.buttons) {
-    if (body.text.includes('{{user_full_name}}') || body.text.includes('[Username]')) {
-      text = text.replace(
-        '{{user_full_name}}', fname + ' ' + lname)
-    }
-    if (body.text.includes('{{user_first_name}}')) {
-      text = text.replace(
-        '{{user_first_name}}', fname)
-    }
-    if (body.text.includes('{{user_last_name}}')) {
-      text = text.replace(
-        '{{user_last_name}}', lname)
-    }
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'attachment': {
-          'type': 'template',
-          'payload': {
-            'template_type': 'button',
-            'text': text,
-            'buttons': buttonPayload
-          }
-        }
-      })
-    }
-  } else if (['image', 'audio', 'file', 'video'].indexOf(
-    body.componentType) > -1) {
-    let dir = path.resolve(__dirname, '../../../../broadcastFiles/userfiles')
-    let fileReaderStream
-    if (body.componentType === 'file') {
-      fileReaderStream = fs.createReadStream(dir + '/' + body.fileurl.name)
     } else {
-      fileReaderStream = fs.createReadStream(dir + '/' + body.fileurl.id)
+      buttonPayload = body.buttons
     }
-
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'attachment': {
-          'type': body.componentType,
-          'payload': {}
-        }
-      }),
-      'filedata': fileReaderStream
-    }
-    return payload
-    // todo test this one. we are not removing as we need to keep it for live chat
-    // if (!isForLiveChat) deleteFile(body.fileurl)
-  } else if (['gif', 'sticker', 'thumbsUp'].indexOf(
-    body.componentType) > -1) {
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'attachment': {
-          'type': 'image',
-          'payload': {
-            'url': body.fileurl
+    if (body.componentType === 'text' && !body.buttons) {
+      if (body.text.includes('{{user_full_name}}') || body.text.includes('[Username]')) {
+        text = text.replace(
+          '{{user_full_name}}', fname + ' ' + lname)
+      }
+      if (body.text.includes('{{user_first_name}}')) {
+        text = text.replace(
+          '{{user_first_name}}', fname)
+      }
+      if (body.text.includes('{{user_last_name}}')) {
+        text = text.replace(
+          '{{user_last_name}}', lname)
+      }
+      payload = {
+        'messaging_type': messageType,
+        'recipient': JSON.stringify({
+          'id': subscriberId
+        }),
+        'message': JSON.stringify({
+          'text': text,
+          'metadata': 'This is a meta data'
+        })
+      }
+      resolve({payload})
+    } else if (body.componentType === 'text' && body.buttons) {
+      if (body.text.includes('{{user_full_name}}') || body.text.includes('[Username]')) {
+        text = text.replace(
+          '{{user_full_name}}', fname + ' ' + lname)
+      }
+      if (body.text.includes('{{user_first_name}}')) {
+        text = text.replace(
+          '{{user_first_name}}', fname)
+      }
+      if (body.text.includes('{{user_last_name}}')) {
+        text = text.replace(
+          '{{user_last_name}}', lname)
+      }
+      payload = {
+        'messaging_type': messageType,
+        'recipient': JSON.stringify({
+          'id': subscriberId
+        }),
+        'message': JSON.stringify({
+          'attachment': {
+            'type': 'template',
+            'payload': {
+              'template_type': 'button',
+              'text': text,
+              'buttons': buttonPayload
+            }
           }
-        }
-      })
-    }
-  } else if (body.componentType === 'card') {
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'attachment': {
-          'type': 'template',
-          'payload': {
-            'template_type': 'generic',
-            'elements': [
-              {
-                'title': body.title,
-                'image_url': body.image_url,
-                'subtitle': body.description,
-                'buttons': buttonPayload
+        })
+      }
+      resolve({payload})
+    } else if (['image', 'audio', 'file', 'video'].indexOf(
+      body.componentType) > -1) {
+      let dir = path.resolve(__dirname, '../../../../broadcastFiles/')
+      let fileToStore = ''
+      if (body.componentType === 'file') {
+        fileToStore = dir + '/userfiles/' + body.fileurl.name
+      } else {
+        fileToStore = dir + '/userfiles/' + body.fileurl.id
+      }
+      var stream = request(`${config.ACCOUNTS_URL}files/download/${body.fileurl.id}`).pipe(fs.createWriteStream(fileToStore))
+      stream.on('finish', function () {
+        console.log('finished')
+        let fileReaderStream = fs.createReadStream(fileToStore)
+        stream.on('close', function () {
+          console.log('finished reading')
+          payload = {
+            'messaging_type': messageType,
+            'recipient': JSON.stringify({
+              'id': subscriberId
+            }),
+            'message': JSON.stringify({
+              'attachment': {
+                'type': body.componentType,
+                'payload': {}
               }
-            ]
+            }),
+            'filedata': fileReaderStream
           }
-        }
+          console.log('in filedata', payload)
+          fs.unlink(fileToStore)
+          resolve({payload})
+        })
       })
-    }
-  } else if (body.componentType === 'gallery') {
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'attachment': {
-          'type': 'template',
-          'payload': {
-            'template_type': 'generic',
-            'elements': body.cards
+      // todo test this one. we are not removing as we need to keep it for live chat
+      // if (!isForLiveChat) deleteFile(body.fileurl)
+    } else if (['gif', 'sticker', 'thumbsUp'].indexOf(
+      body.componentType) > -1) {
+      payload = {
+        'messaging_type': messageType,
+        'recipient': JSON.stringify({
+          'id': subscriberId
+        }),
+        'message': JSON.stringify({
+          'attachment': {
+            'type': 'image',
+            'payload': {
+              'url': body.fileurl
+            }
           }
-        }
-      })
-    }
-  } else if (body.componentType === 'list') {
-    payload = {
-      'messaging_type': messageType,
-      'recipient': JSON.stringify({
-        'id': subscriberId
-      }),
-      'message': JSON.stringify({
-        'attachment': {
-          'type': 'template',
-          'payload': {
-            'template_type': 'list',
-            'top_element_style': body.topElementStyle,
-            'elements': body.listItems,
-            'buttons': buttonPayload
+        })
+      }
+      resolve({payload})
+    } else if (body.componentType === 'card') {
+      payload = {
+        'messaging_type': messageType,
+        'recipient': JSON.stringify({
+          'id': subscriberId
+        }),
+        'message': JSON.stringify({
+          'attachment': {
+            'type': 'template',
+            'payload': {
+              'template_type': 'generic',
+              'elements': [
+                {
+                  'title': body.title,
+                  'image_url': body.image_url,
+                  'subtitle': body.description,
+                  'buttons': buttonPayload
+                }
+              ]
+            }
           }
-        }
-      })
+        })
+      }
+      resolve({payload})
+    } else if (body.componentType === 'gallery') {
+      payload = {
+        'messaging_type': messageType,
+        'recipient': JSON.stringify({
+          'id': subscriberId
+        }),
+        'message': JSON.stringify({
+          'attachment': {
+            'type': 'template',
+            'payload': {
+              'template_type': 'generic',
+              'elements': body.cards
+            }
+          }
+        })
+      }
+      resolve({payload})
+    } else if (body.componentType === 'list') {
+      payload = {
+        'messaging_type': messageType,
+        'recipient': JSON.stringify({
+          'id': subscriberId
+        }),
+        'message': JSON.stringify({
+          'attachment': {
+            'type': 'template',
+            'payload': {
+              'template_type': 'list',
+              'top_element_style': body.topElementStyle,
+              'elements': body.listItems,
+              'buttons': buttonPayload
+            }
+          }
+        })
+      }
+      resolve({payload})
     }
-  }
-  return payload
+  })
 }
 
 function isJsonString (str) {
