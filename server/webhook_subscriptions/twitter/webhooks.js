0038a3c80850c068d484f44bf5faf50b5cd1a2ff
anishachhatwani <i130315@nu.edu.pk>
2019-05-31 13:13:36 +0500

    adding log

--- server/webhook_subscriptions/twitter/webhooks.js
+++ server/webhook_subscriptions/twitter/webhooks.js
@@ -15,7 +15,6 @@ exports.simpleTweet = (payload) => {
 
 exports.mediaTweet = (payload) => {
   logger.serverLog(TAG, `in mediaTweet Webhook ${JSON.stringify(payload)}`)
-  console.log('in twitterWebhook', JSON.stringify(payload))
   callApi.callApi('twitterEvents/twitterAutoposting', 'post', payload, 'kiboengage')
       .then((response) => {
         logger.serverLog(TAG, `response recieved from KiboPush: ${response}`, 'kiboengage')
