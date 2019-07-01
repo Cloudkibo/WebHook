fd21274f197347c6a6619082e424e4123442090c
anishachhatwani <i130315@nu.edu.pk>
2019-07-01 12:34:09 +0500

    adding log

--- server/webhook_subscriptions/twitter/twitter.controller.js
+++ server/webhook_subscriptions/twitter/twitter.controller.js
@@ -66,10 +66,10 @@ function connect () {
   callApi.callApi('twitterEvents/findAutoposting', 'get', {}, 'kiboengage').then((response) => {
     let autoposting = response
     if (autoposting.length > 0) {
-      let arrUsers = []
-      for (let i = 0; i < autoposting.length; i++) {
-        arrUsers.push(autoposting[i].payload.id)
-      }
+      let arrUsers = ['1145580414318383104']
+      // for (let i = 0; i < autoposting.length; i++) {
+      //   arrUsers.push(autoposting[i].payload.id)
+      // }
       logger.serverLog(TAG, `Twitter Ids to listen: ${arrUsers}`, 'debug')
       stream = twitterClient.stream('statuses/filter',
         {follow: arrUsers})
