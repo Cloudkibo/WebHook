b9c429504414f5c7b76606a2b6409f2b63b3e576
anishachhatwani <i130315@nu.edu.pk>
2019-08-23 12:20:56 +0500

    adding logs

--- server/webhooks/logicLayer/createNewSubscriber.js
+++ server/webhooks/logicLayer/createNewSubscriber.js
@@ -29,7 +29,6 @@ exports.createNewSubscriber = (pageId, senderId, subscriberSource, identifier, r
                       if (subscriberFound.length === 0) {
                         callApi(`subscribers`, 'post', payload, 'accounts')
                           .then(subscriberCreated => {
-                            console.log('subscriberCreated', subscriberCreated)
                             if (subscriberSource === 'checkbox_plugin' || subscriberSource === 'shopify') {
                               LogicLayer.sendWebhookForNewSubscriber(
                                 page.pageId,
@@ -58,14 +57,8 @@ exports.createNewSubscriber = (pageId, senderId, subscriberSource, identifier, r
                             }
                             console.log('going to sessions')
                             callApi('messengerEvents/sessions', 'post', {page: page, subscriber: subscriberCreated, event: event}, 'kibochat')
-                              .then(sessRes => {
-                                console.log('sessions response', sessRes)
-                                logger.serverLog(TAG, `response from sessions ${sessRes}`)
-                              })
-                              .catch(err => {
-                                console.log('sessions catch', err)
-                                logger.serverLog(TAG, `Failed to get response from sessions ${JSON.stringify(err)}`, 'error')
-                              })
+                            .then(sessRes => logger.serverLog(TAG, `response from sessions ${sessRes}`))
+                            .catch(err => logger.serverLog(TAG, `Failed to get response from sessions ${JSON.stringify(err)}`, 'error'))
                           })
                           .catch(err => {
                             logger.serverLog(TAG, `Failed to create subscriber ${JSON.stringify(err)}`, 'error')
