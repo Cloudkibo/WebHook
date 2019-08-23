716d8689da889c3bc1b7927f098d27527f77fa3a
anishachhatwani <i130315@nu.edu.pk>
2019-08-23 13:35:04 +0500

    messaging referral

--- server/webhooks/messenger/messengerReferralWebhook.js
+++ server/webhooks/messenger/messengerReferralWebhook.js
@@ -1,11 +1,10 @@
-const TAG = 'webhooks/messenger/newSubscriberWebhook.js'
-const logger = require('../../components/logger')
 const { createNewSubscriber } = require('../logicLayer/createNewSubscriber.js')
 
-exports.newSubscriberWebhook = (payload) => {
+exports.messengerReferralWebhook = (payload) => {
   // logger.serverLog(TAG, `in newSubscriberWebhook ${JSON.stringify(payload)}`, 'debug')
+  console.log('inside messengerReferralWebhook', payload)
   const event = payload.entry[0].messaging[0]
   const senderId = event.message && event.message.is_echo ? event.recipient.id : event.sender.id
   const pageId = event.message && event.message.is_echo ? event.sender.id : event.recipient.id
-  createNewSubscriber(pageId, senderId, 'direct_message', '', null, event)
+  createNewSubscriber(pageId, senderId, 'messaging_referrals', '', null, event)
 }
