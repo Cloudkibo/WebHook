41ebcf9bd9cceac848cf3548b652c217a34b2c7f
anishachhatwani <i130315@nu.edu.pk>
2019-10-28 16:18:20 +0500

    quick repliues

--- server/webhook_subscriptions/twilio/twilio.controller.js
+++ server/webhook_subscriptions/twilio/twilio.controller.js
@@ -84,29 +84,30 @@ exports.sendMessage = function (req, res) {
   let accountSid = 'ACe779810d0b992e2db1b67716bd2b4d80'
   let authToken = '3d3fa38dc27fe0a0262ed97c63d181eb'
   let client = require('twilio')(accountSid, authToken)
+  // let MessageObject = {
+  //   from: `whatsapp:+14155238886`,
+  //   to: `whatsapp:+923322846897`,
+  //   statusCallback: `https://kibopush-anisha.ngrok.io/webhooks/twilio/trackStatusWhatsAppChat/1`
+  // }
+  // if (req.body.payload.componentType !== 'text') {
+  //   MessageObject.mediaUrl = req.body.payload.fileurl.url
+  // } else {
+  //   MessageObject.body = req.body.payload.text
+  // }
+  // client.messages
+  // .create(MessageObject)
+  // .then(response => {
+  //   console.log('response from send message', response)
+  // })
+  // .catch(error => {
+  //   console.log('error at send message', error)
+  // })
   let MessageObject = {
     from: `whatsapp:+14155238886`,
     to: `whatsapp:+923322846897`,
-    statusCallback: `https://kibopush-anisha.ngrok.io/webhooks/twilio/trackStatusWhatsAppChat/1`
-  }
-  if (req.body.payload.componentType !== 'text') {
-    MessageObject.mediaUrl = req.body.payload.fileurl.url
-  } else {
-    MessageObject.body = req.body.payload.text
-  }
-  client.messages
-  .create(MessageObject)
-  .then(response => {
-    console.log('response from send message', response)
-  })
-  .catch(error => {
-    console.log('error at send message', error)
-  })
-  MessageObject = {
-    from: `whatsapp:+14155238886`,
-    to: `whatsapp:+923352134405`,
     statusCallback: `https://kibopush-anisha.ngrok.io/webhooks/twilio/trackStatusWhatsAppChat/1`,
-    body: 'hey'
+    body: 'sample.pdf',
+    mediaUrl: 'https://skibochat.cloudkibo.com/api/broadcasts/download/sample.pdf'
   }
   client.messages
   .create(MessageObject)
--- server/webhooks/messenger/pollResponseWebhook.js
+++ server/webhooks/messenger/pollResponseWebhook.js
@@ -3,8 +3,34 @@ const logger = require('../../components/logger')
 const { callApi } = require('../../utility/api.caller.service')
 
 exports.pollResponseWebhook = (payload) => {
+  console.log('pollResponseWebhook', payload)
   let resp = JSON.parse(payload.entry[0].messaging[0].message.quick_reply.payload)
-  if (resp.option === 'talkToHuman') {
+  console.log('resp value', resp)
+  if (resp.action && resp.action === 'subscribe_to_sequence') {
+    callApi('messengerEvents/subscribeToSequence', 'post', payload, 'kiboengage')
+      .then((response) => {
+        logger.serverLog(TAG, `response recieved from KiboPush: ${response}`, 'debug')
+      })
+      .catch((err) => {
+        logger.serverLog(TAG, `error from KiboPush: ${err}`, 'error')
+      })
+  } else if (resp.action && resp.action === 'unsubscribe_from_sequence') {
+    callApi('messengerEvents/unsubscribeFromSequence', 'post', payload, 'kiboengage')
+      .then((response) => {
+        logger.serverLog(TAG, `response recieved from KiboPush: ${response}`, 'debug')
+      })
+      .catch((err) => {
+        logger.serverLog(TAG, `error from KiboPush: ${err}`, 'error')
+      })
+  } else if (resp.action && resp.action === 'send_new_message') {
+    callApi('messengerEvents/sendTemplateMessage', 'post', payload, 'kiboengage')
+      .then((response) => {
+        logger.serverLog(TAG, `response recieved from KiboPush: ${response}`, 'debug')
+      })
+      .catch((err) => {
+        logger.serverLog(TAG, `error from KiboPush: ${err}`, 'error')
+      })
+  } else if (resp.option && resp.option === 'talkToHuman') {
     // logger.serverLog(TAG,
     //   `in talkToHuman ${JSON.stringify(payload)}`)
     callApi('messengerEvents/talkToHuman', 'post', payload, 'kibochat')
