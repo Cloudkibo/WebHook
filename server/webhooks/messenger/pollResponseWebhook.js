dc70f55578e6a15538d16412ccaa90a47b1b5741
anishachhatwani <i130315@nu.edu.pk>
2019-10-28 16:24:03 +0500

    quick repliues

--- server/webhooks/messenger/pollResponseWebhook.js
+++ server/webhooks/messenger/pollResponseWebhook.js
@@ -3,9 +3,7 @@ const logger = require('../../components/logger')
 const { callApi } = require('../../utility/api.caller.service')
 
 exports.pollResponseWebhook = (payload) => {
-  console.log('pollResponseWebhook', payload)
   let resp = JSON.parse(payload.entry[0].messaging[0].message.quick_reply.payload)
-  console.log('resp value', resp)
   if (resp.action && resp.action === 'subscribe_to_sequence') {
     callApi('messengerEvents/subscribeToSequence', 'post', payload, 'kiboengage')
       .then((response) => {
