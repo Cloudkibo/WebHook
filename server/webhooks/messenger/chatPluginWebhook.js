4dec8af880bc8666389119da64e2d629113bab97
Sojharo <sojharo@live.com>
2019-11-13 13:22:28 +0500

    work on chat plugin logs CloudKibo/KiboPush#7083

--- server/webhooks/messenger/chatPluginWebhook.js
+++ server/webhooks/messenger/chatPluginWebhook.js
@@ -8,7 +8,7 @@ exports.chatPluginWebhook = (payload) => {
   const event = payload.entry[0].messaging[0]
   const senderId = event.message && event.message.is_echo ? event.recipient.id : event.sender.id
   const pageId = event.message && payload.entry[0].messaging[0].message.is_echo ? event.sender.id : event.recipient.id
-  const ref = event.referral && event.referral.ref ? JSON.parse(event.referral.ref) : null
+  const ref = event.referral && event.referral.ref ? JSON.parse(event.referral.ref) : JSON.parse(event.postback.referral.ref)
   createNewSubscriber(pageId, senderId, 'chat_plugin', '', ref, event)
   // callApi('messengerEvents/subscriber', 'post', payload, 'accounts')
   //   .then((response) => {
