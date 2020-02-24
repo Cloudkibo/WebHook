74daf5ed8af33410e23b4c38894c9b70d5f7c8bc
anishachhatwani <i130315@nu.edu.pk>
2020-02-24 14:47:38 +0500

    subscription count

--- server/webhooks/messenger/postbackWebhook.js
+++ server/webhooks/messenger/postbackWebhook.js
@@ -202,54 +202,57 @@ function handleUnsubscribe (resp, req) {
     messageData = {
       text: 'You have unsubscribed from our broadcasts. Send "start" to subscribe again.'
     }
-    callApi(`subscribers/update`, 'put', {query: { senderId: req.sender.id }, newPayload: { isSubscribed: false }, options: {}}, 'accounts')
-      .then(updated => {
-        // logger.serverLog(TAG, `updated subscriber: ${JSON.stringify(updated)}`, 'debug')
-        callApi(`subscribers/query`, 'post', { senderId: req.sender.id }, 'accounts')
-          .then(subscribers => {
-            let subscriber = subscribers[0]
-            callApi(`pages/query`, 'post', { _id: subscriber.pageId._id }, 'accounts')
-              .then(pages => {
-                let page = pages[0]
-                callApi(`tags/query`, 'post', { tag: `_${page.pageId}_unsubscribe`, defaultTag: true, companyId: page.companyId }, 'accounts')
-                  .then(unsubscribeTag => {
-                    unsubscribeTag = unsubscribeTag[0]
-                    // assign tag
-                    needle('post', `https://graph.facebook.com/v2.11/${unsubscribeTag.labelFbId}/label?access_token=${page.accessToken}`, {'user': req.sender.id})
-                      .then(response => {
-                        if (response.body.error) {
-                          logger.serverLog(TAG, `Failed to assign unsubscribeTag ${JSON.stringify(response.body.error)}`, 'error')
-                        } else {
-                          logger.serverLog(TAG, 'unsubscribeTag assigned succssfully!')
-                        }
-                      })
-                      .catch(err => {
-                        logger.serverLog(TAG, `Failed to assign unsubscribeTag ${JSON.stringify(err)}`, 'error')
-                      })
-                  })
-                  .catch(err => {
-                    logger.serverLog(TAG, `Failed to fetch default tag ${JSON.stringify(err)}`, 'error')
-                  })
-              })
-              .catch(err => {
-                logger.serverLog(TAG, `Failed to fetch page ${JSON.stringify(err)}`, 'error')
-              })
-            logger.serverLog(TAG, `subscriber fetched ${JSON.stringify(subscriber)}`, 'debug')
-            callApi('featureUsage/updateCompany', 'put', {query: {companyId: subscriber.companyId}, newPayload: { $inc: { subscribers: -1 } }, options: {}}, 'accounts')
-              .then(updated => {
-                logger.serverLog(TAG, `company usage incremented succssfully ${JSON.stringify(updated)}`, 'debug')
-              })
-              .catch(err => {
-                logger.serverLog(TAG, `Failed to update company usage ${JSON.stringify(err)}`, 'error')
-              })
-          })
-          .catch(err => {
-            logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(err)}`, 'error')
-          })
+    callApi(`pages/query`, 'post', { pageId: req.recipient.id, connected: true }, 'accounts')
+      .then(pages => {
+        let page = pages[0]
+        if (page) {
+          callApi(`subscribers/update`, 'put', {query: { senderId: req.sender.id, pageId: page._id }, newPayload: { isSubscribed: false }, options: {}}, 'accounts')
+            .then(updated => {
+              // logger.serverLog(TAG, `updated subscriber: ${JSON.stringify(updated)}`, 'debug')
+              callApi(`subscribers/query`, 'post', { senderId: req.sender.id, pageId: page._id }, 'accounts')
+                .then(subscribers => {
+                  let subscriber = subscribers[0]
+                  handleNewsSubscription(subscriber)
+                  callApi(`tags/query`, 'post', { tag: `_${page.pageId}_unsubscribe`, defaultTag: true, companyId: page.companyId }, 'accounts')
+                    .then(unsubscribeTag => {
+                      unsubscribeTag = unsubscribeTag[0]
+                      // assign tag
+                      needle('post', `https://graph.facebook.com/v2.11/${unsubscribeTag.labelFbId}/label?access_token=${page.accessToken}`, {'user': req.sender.id})
+                        .then(response => {
+                          if (response.body.error) {
+                            logger.serverLog(TAG, `Failed to assign unsubscribeTag ${JSON.stringify(response.body.error)}`, 'error')
+                          } else {
+                            logger.serverLog(TAG, 'unsubscribeTag assigned succssfully!')
+                          }
+                        })
+                        .catch(err => {
+                          logger.serverLog(TAG, `Failed to assign unsubscribeTag ${JSON.stringify(err)}`, 'error')
+                        })
+                    })
+                    .catch(err => {
+                      logger.serverLog(TAG, `Failed to fetch default tag ${JSON.stringify(err)}`, 'error')
+                    })
+                  logger.serverLog(TAG, `subscriber fetched ${JSON.stringify(subscriber)}`, 'debug')
+                  callApi('featureUsage/updateCompany', 'put', {query: {companyId: subscriber.companyId}, newPayload: { $inc: { subscribers: -1 } }, options: {}}, 'accounts')
+                    .then(updated => {
+                      logger.serverLog(TAG, `company usage incremented succssfully ${JSON.stringify(updated)}`, 'debug')
+                    })
+                    .catch(err => {
+                      logger.serverLog(TAG, `Failed to update company usage ${JSON.stringify(err)}`, 'error')
+                    })
+                })
+                .catch(err => {
+                  logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(err)}`, 'error')
+                })
+            })
+            .catch(err => {
+              logger.serverLog(TAG, `Failed to update subscriber ${JSON.stringify(err)}`, 'error')
+            })
+        }
       })
-      .catch(err => {
-        logger.serverLog(TAG, `Failed to update subscriber ${JSON.stringify(err)}`, 'error')
-      })
+    .catch(err => {
+      logger.serverLog(TAG, `Failed to fetch page ${JSON.stringify(err)}`, 'error')
+    })
   } else {
     messageData = {
       text: 'You can unsubscribe anytime by saying stop.'
@@ -275,6 +278,73 @@ function handleUnsubscribe (resp, req) {
             `Sending unsubscribe confirmation response to subscriber  ${JSON.stringify(
               respp.body)}`, 'debug')
         })
+    })
+}
+
+function handleNewsSubscription (subscriber) {
+  let defaultQuery = {
+    purpose: 'findAll',
+    match: {defaultFeed: true, companyId: subscriber.companyId}
+  }
+  let subscriptionQuery = {
+    purpose: 'findAll',
+    match: {'subscriberId._id': subscriber._id}
+  }
+  callApi(`newsSections/query`, 'post', defaultQuery, 'engageDbLayer')
+    .then(defaultNewsSections => {
+      callApi(`newsSubscriptions/query`, 'post', subscriptionQuery, 'engageDbLayer')
+        .then(newsSubscriptions => {
+          if (newsSubscriptions.length > 0) {
+            let subscriptionIds = newsSubscriptions.filter(n => n.subscription).map(s => s._id)
+            let newsIds = newsSubscriptions.filter(a => a.subscription).map(n => n.newsSectionId)
+            updateSubscription({_id: {$in: subscriptionIds}})
+            updateSubscriptionCount({_id: {$in: newsIds}})
+            let defaultSubscriptions = []
+            let defaultNewsSectionIds = defaultNewsSections.map(a => a._id)
+            let newsSubscriptionsIds = newsSubscriptions.map(n => n._id)
+            defaultSubscriptions = defaultNewsSectionIds.filter(function (v) { return newsSubscriptionsIds.indexOf(v) >= 0 })
+            defaultSubscriptions.concat(newsSubscriptionsIds.filter(function (v) { return defaultNewsSectionIds.indexOf(v) >= 0 }))
+            if (defaultSubscriptions.length === 0) {
+              updateSubscriptionCount({_id: {$in: defaultNewsSectionIds}})
+            }
+          } else {
+            updateSubscriptionCount({defaultFeed: true, companyId: subscriber.companyId})
+          }
+        })
+        .catch(err => {
+          logger.serverLog(TAG, `Failed to fetch subscriptions ${JSON.stringify(err)}`, 'error')
+        })
+    })
+  .catch(err => {
+    logger.serverLog(TAG, `Failed to default feeds ${err}`, 'error')
+  })
+}
+
+function updateSubscriptionCount (query) {
+  let updateQuery = {
+    purpose: 'updateAll',
+    match: query,
+    updated: {$inc: {subscriptions: -1}}
+  }
+  callApi(`newsSections`, 'put', updateQuery, 'engageDbLayer')
+    .then(updated => {
+    })
+    .catch(err => {
+      logger.serverLog(TAG, `Failed to udpate subscription count for default ${JSON.stringify(err)}`, 'error')
+    })
+}
+
+function updateSubscription (query) {
+  let updateQuery = {
+    purpose: 'updateAll',
+    match: query,
+    updated: {subscription: false}
+  }
+  callApi(`newsSubscriptions`, 'put', updateQuery, 'engageDbLayer')
+    .then(updated => {
+    })
+    .catch(err => {
+      logger.serverLog(TAG, `Failed to udpate subscriptions ${JSON.stringify(err)}`, 'error')
     })
 }
 
