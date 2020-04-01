6f79266edeae06ff4f8330e192eac8a627999441
Baqar <bjafri5@gmail.com>
2020-04-01 12:35:57 +0500

    log

--- server/api/v1/messenger/messenger.controller.js
+++ server/api/v1/messenger/messenger.controller.js
@@ -60,6 +60,7 @@ function webhookHandler (body) {
   let webhookCalled = false
   init.getRegistry().map((entry) => {
     if (validator.validate(body, entry.schema).valid && !webhookCalled) {
+      console.log('webhook entry', entry)
       entry.callback(_cloneDeep(body))
       webhookCalled = true
     }
