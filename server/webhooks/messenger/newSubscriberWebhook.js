01266680d527e51f754fc70016f5694a68a3de3d
Baqar <bjafri5@gmail.com>
2020-04-01 12:30:04 +0500

    logs

--- server/webhooks/messenger/newSubscriberWebhook.js
+++ server/webhooks/messenger/newSubscriberWebhook.js
@@ -3,7 +3,7 @@ const logger = require('../../components/logger')
 const { createNewSubscriber } = require('../logicLayer/createNewSubscriber.js')
 
 exports.newSubscriberWebhook = (payload) => {
-  // logger.serverLog(TAG, `in newSubscriberWebhook ${JSON.stringify(payload)}`, 'debug')
+  logger.serverLog(TAG, `in newSubscriberWebhook ${JSON.stringify(payload)}`, 'debug')
   const event = payload.entry[0].messaging[0]
   const senderId = event.message && event.message.is_echo ? event.recipient.id : event.sender.id
   const pageId = event.message && event.message.is_echo ? event.sender.id : event.recipient.id
