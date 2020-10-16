dc3c4324d4413da72731f5d35f65a41cca0ddd90
anishachhatwani <i130315@nu.edu.pk>
2020-10-16 16:15:51 +0500

    webhook v2

--- server/api/global/twilio.js
+++ server/api/global/twilio.js
@@ -2,7 +2,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse
 
 exports.sendResponseToTwilio = (res) => {
   const twiml = new MessagingResponse()
-  twiml.message('')
+  twiml.message('Thanks')
 
   res.writeHead(200, { 'Content-Type': 'text/xml' })
   res.end(twiml.toString())
