2a54b7cc10f390a502724bc958455e791b7ff4e5
saniasiddiqui <31842853+saniasiddiqui@users.noreply.github.com>
2021-01-14 10:19:39 +0500

    Update messageReceivedWebhook.js

--- server/webhooks/cequens/messageReceivedWebhook.js
+++ server/webhooks/cequens/messageReceivedWebhook.js
@@ -1,7 +1,6 @@
 const { callApi } = require('../../utility/api.caller.service')
 
 exports.messageReceivedWebhook = (payload) => {
-  console.log('In messagereceived webhook', payload)
   let data = {
     provider: 'cequens',
     event: payload
