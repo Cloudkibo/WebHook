c91d189071864ff041fe8ef3f40e3c8f13ae9d01
Baqar <bjafri5@gmail.com>
2021-01-29 15:55:16 +0500

    fix

--- server/webhooks/logicLayer/createNewSubscriber.logiclayer.js
+++ server/webhooks/logicLayer/createNewSubscriber.logiclayer.js
@@ -12,11 +12,22 @@ exports.getSubscriberInfoFromFB = (sender, pageAccessToken, page) => {
       method: 'GET'
     }
     needle.get(options.url, options, (error, response) => {
-      logger.serverLog(`Subscriber response git from facebook`, `${TAG}: exports.getSubscriberInfoFromFB`, {}, {response: response.body, page: page}, 'debug')
       if (error) {
         reject(error)
       } else {
-        resolve(response)
+        // handling "This action was not submitted due to new privacy rules in Europe." error
+        if (response.body.error && response.body.error.code === 10 && response.body.error.subcode === 2018336) {
+          options.url = `https://graph.facebook.com/v6.0/${sender}?fields=gender,first_name,last_name,locale,timezone&access_token=${pageAccessToken}`
+          needle.get(options.url, options, (error, response) => {
+            if (error) {
+              reject(error)
+            } else {
+              resolve(response)
+            }
+          })
+        } else {
+          resolve(response)
+        }
       }
     })
   })
